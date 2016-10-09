c46ec0736927aa3113b7b20d0cf250eb533faac6
mtoto_dev <mtoto.mgonjwa@gmail.com>
2016-10-09 15:54:49 +0300

    database changes

--- models/database.js
+++ models/database.js
@@ -1,14 +1,15 @@
 var Sequelize = require('sequelize');
 
-var connection = new Sequelize('sos_website', 'root', 'root'
-/*,
+var connection = new Sequelize('sos_website', 'tom', 'tom'
+
+,
 {
   //this is for when we host this website.
   host: 'localhost',
   //Kipesh Should uncomment this line since he wants to use postgres
   dialect: 'postgres'
 }
-*/);
+);
 
 var Events = connection.define('events', {
   title:
