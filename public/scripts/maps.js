dd101714a60c7de535e59b1bb777611312a7d7f9
mtoto_dev <mtoto.mgonjwa@gmail.com>
2016-10-27 19:35:15 +0300

    Daystar University Markers

--- public/scripts/maps.js
+++ public/scripts/maps.js
@@ -27,7 +27,7 @@ function initMap() {
     }, {"featureType": "road", "elementType": "geometry", "stylers": [{"color": "#454545"}]}];
     // Constructor creates a new map - only center and zoom are required.
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -1.4414666, lng: 37.04677852},
+        center: {lat: -1.44460, lng: 37.04677852},
         zoom: 16,
         // styles:styles,
         mapTypeId: "OSM",
@@ -44,12 +44,53 @@ function initMap() {
         name: "OpenStreetMap",
         maxZoom: 20
     }));
-    var tribeca = {lat: -1.4414666, lng: 37.04677852};
-    var marker = new google.maps.Marker({
-        position: tribeca,
-        map: map,
-        title: 'Daystar Universtity!'
-    });
+    // These are the real estate listings that will be shown to the user.
+    // Normally we'd have these in a database instead.
+    var locations = [
+        {title: 'Daystar University Athi River', location: {lat: -1.44460, lng: 37.04959}},
+        {title: 'Daystar University Valley Road', location: {lat: -1.29718, lng: 36.80276}}
+    ];
+    var largeInfowindow = new google.maps.InfoWindow();
+    var bounds = new google.maps.LatLngBounds();
+    // The following group uses the location array to create an array of markers on initialize.
+    for (var i = 0; i < locations.length; i++) {
+        // Get the position from the location array.
+        var position = locations[i].location;
+        var title = locations[i].title;
+        // Create a marker per location, and put into markers array.
+        var marker = new google.maps.Marker({
+            map: map,
+            position: position,
+            title: title,
+            animation: google.maps.Animation.DROP,
+            id: i
+        });
+        // Push the marker to our array of markers.
+        markers.push(marker);
+        // Create an onclick event to open an infowindow at each marker.
+        marker.addListener('click', function() {
+            populateInfoWindow(this, largeInfowindow);
+        });
+        bounds.extend(markers[i].position);
+    }
+    // Extend the boundaries of the map for each marker
+    map.fitBounds(bounds);
+}
+// This function populates the infowindow when the marker is clicked. We'll only allow
+// one infowindow which will open at the marker that is clicked, and populate based
+// on that markers position.
+function populateInfoWindow(marker, infowindow) {
+    // Check to make sure the infowindow is not already opened on this marker.
+    if (infowindow.marker != marker) {
+        infowindow.marker = marker;
+        infowindow.setContent('<div>' + marker.title + '</div>');
+        infowindow.open(map, marker);
+        // Make sure the marker property is cleared if the infowindow is closed.
+        infowindow.addListener('closeclick',function(){
+            infowindow.setMarker(null);
+        });
+    }
+
 
 
 }
