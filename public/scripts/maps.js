e06ea91960e7d889db7b7edb3b4d2a5180a5a83b
mtoto_dev <mtoto.mgonjwa@gmail.com>
2016-10-27 14:36:48 +0300

    Merge branch 'master' of https://github.com/dita-dev-team/sos-website into mtoto_dev
    # Conflicts:
    #	public/stylesheets/style.css
    #	views/index.pug
    #	views/layout.pug
    # Maps On Daystar University

--- public/scripts/maps.js
+++ public/scripts/maps.js
@@ -26,11 +26,23 @@ function initMap() {
     }, {"featureType": "road", "elementType": "geometry", "stylers": [{"color": "#454545"}]}];
     // Constructor creates a new map - only center and zoom are required.
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -1.439590, lng: 37.046964},
-        zoom: 13,
-        styles: styles,
-        mapTypeControl: false
+        center: {lat: -1.4414666, lng: 37.04677852},
+        zoom: 16,
+        // styles:styles,
+        mapTypeId: "OSM",
+        mapTypeControl: false,
+        streetViewControl: false
     });
+
+    //Define OSM map type pointing at the OpenStreetMap tile server
+    map.mapTypes.set("OSM", new google.maps.ImageMapType({
+        getTileUrl: function(coord, zoom) {
+            return "http://tile.openstreetmap.org/" + zoom + "/" + coord.x + "/" + coord.y + ".png";
+        },
+        tileSize: new google.maps.Size(256, 256),
+        name: "OpenStreetMap",
+        maxZoom: 20
+    }));
     // These are the real estate listings that will be shown to the user.
     // Normally we'd have these in a database instead.
     var locations = [
