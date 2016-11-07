45af68741ea9fcbc984cc197a8e877946eaceb80
Michael Bukachi <michaelbukachi@gmail.com>
2016-11-07 13:46:06 +0300

    Minor improvements to events section

--- models/demo.js
+++ models/demo.js
@@ -5,7 +5,9 @@ db.Events.destroy({truncate: true}).catch(function (errors) {
     console.log(errors);
 });
 
-for (i = 0; i < 4; i++) {
+var n = Math.random() * (4 - 1) + 1;
+
+for (i = 0; i < n; i++) {
     var title = 'Title ' + (i + 1);
     var venue = 'ICT';
     var description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
