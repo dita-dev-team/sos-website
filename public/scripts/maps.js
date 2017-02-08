a7a2a275e3f21d38f1bbeae8305a212de77f04fb
mtotomgonjwa <mtoto.mgonjwa@gmail.com>
2017-02-08 18:44:14 +0300

    Updated maps.js

--- public/scripts/maps.js
+++ public/scripts/maps.js
@@ -1,19 +1,52 @@
-var mymap = L.map('map').setView([-1.4431146, 37.0490923], 14);
+var map = L.map('map');
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/mtotomgonjwa.2of0mcnl/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibXRvdG9tZ29uandhIiwiYSI6ImNpeWVpeXJzMzAwOTEzM3FwcDV5bnY5NWsifQ.zZ_ILTdPpKgrwQyd7p88pg', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     id: 'mtotomgonjwa.2of0mcnl',
     accessToken: 'pk.eyJ1IjoibXRvdG9tZ29uandhIiwiYSI6ImNpeWVpeXJzMzAwOTEzM3FwcDV5bnY5NWsifQ.zZ_ILTdPpKgrwQyd7p88pg'
-}).addTo(mymap);
+}).addTo(map);
 
 
-var marker = L.marker([-1.4431146, 37.0490923]).addTo(mymap);
-marker.bindPopup("<b>Hello world!</b><br>Welcome to Daystar Athi River Campus");
-var marker = L.marker([-1.2969668,36.8018029]).addTo(mymap);
-marker.bindPopup("<b>Hello world!</b><br>Welcome to Daystar Valley Road Campus");
+(function () {
+    map.setView([-1.3837743,36.9367862], 11);
+    var markers = [];
 
-mymap.fitBounds([
-    [-1.4431146, 37.0490923],
-    [-1.2969668,36.8018029]
-]);
+    var locations = [
+        {title: 'Daystar University Athi River', location: {lat: -1.4431146, lng: 37.0490923}},
+        {title: 'Daystar University Valley Road', location: {lat: -1.2969668, lng: 36.8018029}}
+    ];
+    //Plot Markers.
+
+    for (var i = 0; i < locations.length; i++) {
+        // Get the position from the location array.
+        var position = locations[i].location;
+        var title = locations[i].title;
+
+        var popupText = '<strong>' + title + '</strong> <br>  on  location. Coordinates: (' + position + ')';
+        var marker = new L.Marker(position);
+
+        marker.bindPopup(popupText);
+
+        marker.on('mouseover', function (e) {
+            this.openPopup();
+        });
+        marker.on('mouseout', function (e) {
+            this.closePopup();
+        });
+
+        markers.push(marker);
+    }
+    var daystarcampuses = L.layerGroup(markers);
+
+    map.addLayer(daystarcampuses);
+
+
+    var overlayMaps = {
+        "Daystar University": daystarcampuses
+    };
+
+    L.control.layers(null, overlayMaps).addTo(map);
+
+
+})();
